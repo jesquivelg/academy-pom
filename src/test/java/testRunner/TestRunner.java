88f88b0999cf58118d89f148fafd9f160d46a52f
jacob <jesquivelg>
2019-11-15 12:40:21 -0600

    cart feature

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -3,7 +3,7 @@ package testRunner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
-		features = "src/test/resources/features/login.feature", 
+		features = "src/test/resources/features/cart.feature", 
 		glue = {"stepDefinitions"},
 		tags = {"not @ignore"}
 		
